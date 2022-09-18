88c8a24e89ae3549368f3ea472110220fa785009
Sigmund Howe <sig@howeville.com>
2022-09-18 22:53:57 +0800

    Added thing

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,6 @@
 //import all libraries
 const { contextBridge, ipcRenderer } = require('electron');
 const Quill = require('../node_modules/quill/dist/quill.js');
-const CalEvent = require("./calEvent.js");
 
 //Wait for page to load in
 document.addEventListener('DOMContentLoaded', function(){
@@ -9,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
   //Initialise variables
   let notePadContent = "";
   let currentPage = 1;
+  const eventList = [];
 
   //find and label all relevant site content
   var notePad = new Quill('#notepad', {
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function(){
   let prevButton = document.getElementById("prevButton");
   let nextButton = document.getElementById("nextButton");
   let changeButton = document.getElementById("changeButton");
-
+  let scheduleBox = document.getElementById("schedulebox");
 
   //Add functionality to saveButton
   saveButton.addEventListener("click", () => {
@@ -52,6 +52,58 @@ document.addEventListener('DOMContentLoaded', function(){
 
   //Initialize Notebook content
   ipcRenderer.send("loadText", currentPage);
+
+  //Initialize eventList[] with a bunch of dummy text
+  function addEvent(type, day, month, year, hour, minute, content, association){
+    let newEvent = [];
+    newEvent[0] = content;
+    newEvent[1] = day;
+    newEvent[2] = month;
+    newEvent[3] = year;
+    newEvent[4] = association;
+    newEvent[5] = null;
+    newEvent[6] = hour;
+    newEvent[7] = minute
+    newEvent[8] = type;
+    eventList.push(newEvent);
+  }
+
+  addEvent("deadline", 19, 9, 2022, 23, 59, "Presentation for hackathon", ["default", 1]);
+  addEvent("deadline", 20, 9, 2022, 1, 55, "Japanese writing test", ["default", 1]);
+
+  //Populate display with all events stored in eventList[]
+  for(var x = 0; x < eventList.length; x++){
+    let child = document.createElement("div");
+    eventList[x][5] = scheduleBox.appendChild(child);
+    eventList[x][5].classList.add("schedule-event");
+    child = document.createElement("div");
+    child.classList.add("date");
+    child.innerHTML = eventList[x][1] + "/" + eventList[x][2] + "/" + eventList[x][3];
+    if(eventList[x][6] < 10){
+      child.innerHTML += " 0" + eventList[x][6] + ":"
+    }
+    else {
+      child.innerHTML += " " + eventList[x][6] + ":"
+    }
+    if(eventList[x][7] < 10){
+       child.innerHTML += "0" + eventList[x][7];
+    }
+    else{
+      child.innerHTML += eventList[x][7];
+    }
+
+    eventList[x][5].appendChild(child);
+    child = document.createElement("div");
+    child.classList.add("event-content");
+    child.innerHTML = eventList[x][0];
+    eventList[x][5].appendChild(child);
+    eventList[x][5].classList.add(eventList[x][8]);
+  }
+
+
+
+
+
 
 
 });
